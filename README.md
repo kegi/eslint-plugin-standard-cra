0c42242c000e63452873ce3c24db6407f3760fc2
Kevin Gilbert <kevin@kegisiteweb.com>
2021-03-28 13:24:31 -0400

    Updated doc and fixes useless rules

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # eslint-plugin-standard-crats
 > **StandardJS for CRA + TS**
-> 
-> This ESLint plugin extends StandardJS for TypeScript and include a set of strict rules for JSX. This plugin is designed for React CRA.
+
+## Overview
+- Designed for React CRA
+- Designed for TypeScript
+- Extends StandardJS
+- Comma Dangling enabled
+- Strict rules for React and JSX
 
 ## Installation
 
-1) Install the plugin on your CRA app (with TypeScript)
-```
+1) Install
+```bash
 yarn add eslint-plugin-standard-crats --dev
 # OR
 npm install eslint-plugin-standard-crats --dev
 ```
-2) Add `.eslintrc.js` at the root of your project
+
+2) Add `.eslintrc.js` at the root of your react project
 ```javascript
 module.exports = {
   extends: [
@@ -19,5 +25,85 @@ module.exports = {
   ],
 }
 ```
-## How to use ?
-todo
+
+3) Add this in `package.json` > `scripts` :
+```json
+{
+  "scripts": {
+    "lint": "eslint src",
+    "lint:fix": "eslint --fix src",
+  }
+}
+```
+
+### VSCode
+Install [EsLint extension](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+
+Add this in `.vscode/settings.json`
+```json
+{
+  "eslint.format.enable": true,
+  "editor.defaultFormatter": "dbaeumer.vscode-eslint",
+}
+```
+
+## Rules
+
+1) [Recommended rules from React plugin](https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules)
+2) [StandardJS](https://standardjs.com/) (+TypeScript rules)
+3) See rules list below
+
+# Strict React+JSX rules
+
+## React components
+- 🔴 React files need extension `.tsx`
+- 🔴 `arrow-functions` are required for all components
+- 🔴 Component name need to be in Pascal case (ex: `<MyComponentName />`)
+- 🔴 No [dangerous](https://reactjs.org/docs/dom-elements.html)  properties
+- 🔴 No children in void DOM element. (ex: `<br> forbidden </br>`)
+- 🔴 React fragments need to be simplified. (ex: `<> Allowed </>`)
+- 🟡 Only 1 nested component per line
+- 🟡 No useless closing tag (ex: `<Foo></Foo>`)
+- 🟡 No useless fragments
+
+## React component props
+- 🔴 No URL starting with `javascript:` in href prop
+- 🟡 No single quote for props
+- 🟡 No `style` prop
+- 🟡 Array indexes in `key` prop
+- 🟡 No useless Boolean prop (ex: `<Comp forbidden={true} />`)
+- 🟡 No useless curly braces in props (ex: `<Comp forbidden={'string'} />`)
+- 🟡 Maximum 2 props per line
+
+## Indentation
+- 🟡 2 spaces indentation
+- 🟡 Multiple indentation rules, see below:
+
+```typescript
+const MyComponent: React.FC<MyComponentProps> = ({
+  foo,
+  bar,
+  ...props
+}) => {
+  return (
+    <>
+      <span id="id" className="myClass">
+        text
+        <strong>bold</strong>
+      </span>
+
+      { ' legal' }
+
+      { foo && (
+        <FooComponent
+          data-foo="foo"
+          bar={bar}
+          baz
+          onClick={() => { handleClick() }}
+          {...props}
+        />
+      ) }
+    </>
+  )
+}
+```
--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@
  'use strict'
 
  const cratsBase = {
-  plugins: [
-    'react',
-    'react-hooks',
-  ],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-  ],
+   plugins: [
+     'react',
+     'react-hooks',
+   ],
+   parserOptions: {
+     project: './tsconfig.json',
+   },
+   extends: [
+     'plugin:react/recommended',
+     'standard-with-typescript',
+   ],
+   settings: {
+     react: {
+       version: 'detect',
+     },
+   },
  }
  
  module.exports = {
@@ -24,37 +29,37 @@
      recommended: {
        ...cratsBase,
        rules: {
-         'comma-dangle': ['error', 'always-multiline'],
- 
+         'comma-dangle': ['warn', 'always-multiline'],
+
+         'jsx-quotes': ['warn', 'prefer-double'],
+
          'react/forbid-component-props': ['warn', { forbid: ['style'] }],
          'react/function-component-definition': ['error', { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' }],
          'react/no-array-index-key': 'error',
          'react/no-danger': 'error',
-         'react/no-unsafe': 'error',
          'react/prop-types': 'off', // not needed with typescript
-         'react/react-in-jsx-scope': 'off', // not needed in react 17
+         'react/react-in-jsx-scope': 'off', // not needed with react 17
          'react/self-closing-comp': ['warn', { component: true, html: true }],
          'react/void-dom-elements-no-children': 'error',
- 
+
          'react/jsx-boolean-value': ['warn', 'never'],
          'react/jsx-closing-bracket-location': ['warn', 'line-aligned'],
          'react/jsx-closing-tag-location': 'warn',
          'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
          'react/jsx-curly-newline': ['warn', { multiline: 'consistent', singleline: 'consistent' }],
-         'react/jsx-curly-spacing': ['warn', { when: 'never' }],
+         'react/jsx-curly-spacing': ['warn', { when: 'never', children: { when: 'always' } }],
          'react/jsx-equals-spacing': ['warn', 'never'],
-         'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+         'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
          'react/jsx-first-prop-new-line': ['warn', 'multiline-multiprop'],
-         'react/jsx-fragments': ['warn', 'syntax'],
+         'react/jsx-fragments': ['error', 'syntax'],
          'react/jsx-indent': ['warn', 2, { checkAttributes: true, indentLogicalExpressions: true }],
          'react/jsx-indent-props': ['warn', 2],
-         'react/jsx-max-props-per-line': ['warn', { maximum: 1, when: 'always' }],
+         'react/jsx-max-props-per-line': ['warn', { maximum: 2, when: 'always' }],
          'react/jsx-no-script-url': 'error',
          'react/jsx-no-useless-fragment': 'warn',
          'react/jsx-one-expression-per-line': ['warn', { allow: 'single-child' }],
          'react/jsx-pascal-case': 'error',
          'react/jsx-props-no-multi-spaces': 'warn',
-         'react/jsx-space-before-closing': ['warn', 'always'],
          'react/jsx-tag-spacing': ['warn', {
            closingSlash: 'never',
            beforeSelfClosing: 'always',
@@ -70,13 +75,10 @@
            logical: 'parens-new-line',
            prop: 'parens-new-line',
          }],
- 
+
          '@typescript-eslint/default-param-last': 'off',
-         '@typescript-eslint/no-dynamic-delete': 'off',
          '@typescript-eslint/explicit-function-return-type': 'off',
          '@typescript-eslint/strict-boolean-expressions': 'off',
-         '@typescript-eslint/prefer-optional-chain': 'off', // not building correctly with esnext
-         '@typescript-eslint/prefer-nullish-coalescing': 'off', // not building correctly with esnext
        },
      },
    },
